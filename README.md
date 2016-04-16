ef5d248f2959e314e6f8f39c349da0cad225f261
Peter Hu <yesky93@sina.com>
2016-04-16 21:01:37 +0800

    fork and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+##zer_day.py
+计算儿子的生辰天数
+
+##fork.py
+- pid=os.fork() #child-process pid is 0
+- two ways to express the if...else..the using the and-or trick
+n
---
+++ fork.py
@@ -0,0 +1,22 @@
+#coding=utf8
+
+import os
+from time import sleep
+
+count = 10
+
+try:
+	pid = os.fork()
+	if pid == 0: #child process , pid is 0
+		print "this is child"
+		count = count - 1
+		sleep(3)
+	else: #parent process
+		print "this is parent"
+		sleep(10)
+
+	#print "the count", count, ' from ', "child" if pid == 0 else "parent"
+	print "the count", count, ' from ', "child" , pid==0 and "child" or "parent"
+except OSError, e:
+	pass
+
